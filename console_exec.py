9ca603a800ec96ec1bf932f9f5ce61e543a9a4eb
Mustafa Yalçın <myalcin81@users.noreply.github.com>
2020-08-02 20:12:49 +0300

    support Mac OS iTerm2

--- console_exec.py
+++ console_exec.py
@@ -3,14 +3,12 @@
 """
 Console Exec
 
-Plugin for Sublime Text 2 to execute a command and redirect its output
+Plugin for Sublime Text 2 & 3 to execute a command and redirect its output
 into a console window. This is based on the default exec command.
 """
 
-import os
-import subprocess
-import sublime
 import sublime_plugin
+from sys import platform
 
 try:
     from shlex import quote
@@ -20,20 +18,36 @@ except ImportError:
 
 class ConsoleExecCommand(sublime_plugin.WindowCommand):
     def run(self, cmd=[], env={}, path='', shell=False, win_console=None,
-            unix_console=None, **kwargs):
+            unix_console=None, mac_console=None, **kwargs):
         # Show message
         sublime.status_message('Running ' + ' '.join(cmd))
+        # sublime.message_dialog('this is a test')
+        print('os name: ' + os.name)
+        print('platform: ' + platform)
 
         # Get platform-specific command arguments
-        if os.name == 'nt':
+        if platform == 'win32' or platform == 'cygwin':
             console = win_console or ['cmd.exe', '/c']
             pause = ['pause']
             console_cmd = console + cmd + ['&'] + pause
-        else:
+        elif platform == 'linux' or platform == 'linux2':
             console = unix_console or ['xterm', '-e']
             escaped_cmd = ' '.join(quote(x) for x in cmd)
             pause = 'bash -c \'read -p "Press [Enter] to continue..."\''
             console_cmd = console + ['{} ; {}'.format(escaped_cmd, pause)]
+        elif platform == 'darwin':
+            print('running OSX command')
+            # console = mac_console or ['bash', '-e']
+            console = mac_console or ['open', '-a', 'Terminal.app']
+            escaped_cmd = ' '.join(quote(x) for x in cmd)
+            pause = 'bash -c \'read -p "Press [Enter] to continue..."\''
+            # console_cmd = console + ['{} ; {}'.format(escaped_cmd, pause)]
+            # console_cmd = console
+            console_cmd = ['osascript', '-e', ('tell application "Terminal"\n    tell application "Terminal" to do script "{0}"\n    activate\nend tell').format(escaped_cmd)]
+        else:
+            sublime.message_dialog('Console Exec: It appears your platform is not supported, please submit a GitHub issue')
+
+
 
         # Default the to the current file's directory if no working directory
         # was provided
